9c32a8d608f5ff7d9c4a03e95c9794f882b84954
PatrickMeireles <patricksmamorim@hotmail.com>
2020-03-28 12:07:07 -0300

    Refatoração e isolamento de funções da Timeline

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import Pubsub from 'pubsub-js';
 
 export default class Header extends Component {
+
+    pesquisa(event){
+        event.preventDefault();
+
+        let urlPesquisa;
+
+        if(this.loginPesquisado.value === '')
+            urlPesquisa = `https://instalura-api.herokuapp.com/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
+        else
+            urlPesquisa = `https://instalura-api.herokuapp.com/api/public/fotos/${this.loginPesquisado.value}`;
+
+        fetch(urlPesquisa)
+        .then(response => response.json())
+        .then(fotos => {            
+            Pubsub.publish('timeline', { fotos });
+        });
+    }
 
     render(){
         return (
@@ -9,8 +27,8 @@ export default class Header extends Component {
                     Instalura
                 </h1>
 
-                <form className="header-busca">
-                    <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo"/>
+                <form className="header-busca" onSubmit={this.pesquisa.bind(this)}>
+                    <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo" ref={input => this.loginPesquisado = input}/>
                     <input type="submit" value="Buscar" className="header-busca-submit"/>
                 </form>
 
--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import FotoItem from './Foto';
+import PubSub from 'pubsub-js';
+import  { CSSTransitionGroup as ReactCSSTransitionGroup } from 'react-transition-group';
 
 export default class Timeline extends Component {
 
@@ -8,6 +10,34 @@ export default class Timeline extends Component {
 
         this.state = { fotos: [] };
         this.login = this.props.login;
+    }
+
+    componentWillMount(){
+        PubSub.subscribe('timeline', (topico, fotos)=> {
+            this.setState({fotos: fotos.fotos})
+        })
+
+        PubSub.subscribe('atualiza-liker', (topico, infoLiker) => {
+            const fotoFind = this.state.fotos.find(foto => foto.id == infoLiker.fotoId);
+            fotoFind.likeada = !fotoFind.likeada;
+            const possivelLiker = fotoFind.likers.find(liker => liker.login == infoLiker.liker.login);
+            if (possivelLiker === undefined) {
+                fotoFind.likers.push(infoLiker.liker);
+            }
+            else {
+                const novosLikers = fotoFind.likers.filter(liker => liker.login !== infoLiker.liker.login);
+                fotoFind.likers = novosLikers;
+            }
+            this.setState({ fotos: this.state.fotos });
+        }
+        )
+      
+        PubSub.subscribe('novos-comentarios', (topico, infoComentario) => {
+            const fotoFind = this.state.fotos.find(com => com.id == infoComentario.fotoId);
+            fotoFind.comentarios.push(infoComentario.novoComentario);
+            this.setState({ fotos: this.state.fotos });
+        });
+
     }
 
     carregaFotos(){
@@ -36,12 +66,56 @@ export default class Timeline extends Component {
         }
     }
 
+    like(fotoId) {
+        //OU X-AUTH-TOKEN VIA HEADER
+        fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`,
+            { method: 'POST' })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                else {
+                    throw new Error('Não foi possível realizar o like da foto');
+                }
+            })
+            .then(liker => {                
+                PubSub.publish('atualiza-liker', { fotoId: fotoId, liker });
+            });
+    }
+
+    comenta(fotoId, comentario){
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify({ texto: comentario }),
+            headers: new Headers({ 'Content-type': 'application/json' })
+          }
+      
+          fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`,
+            requestInfo)
+            .then(response => {
+              if (response.ok) {
+                return response.json();
+              }
+              else {
+                throw new Error('Não foi possível comentar');
+              }
+            }).
+            then(novoComentario => {
+              PubSub.publish('novos-comentarios', { fotoId: fotoId, novoComentario })
+            });
+    }
+
     render() {
         return (
             <div className="fotos container">
-                {
-                    this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} />)
-                }
+                <ReactCSSTransitionGroup
+                    transitionName="timeline"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={300}>
+                    {
+                        this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like} comenta={ this.comenta }/>)
+                    }
+                </ReactCSSTransitionGroup>
             </div>
         )
     }
